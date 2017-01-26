52d8d1715b69eae0d948338969808188511ae91f
liavt <liavt@yahoo.com>
2017-01-26 09:06:03 -0600

    Mjpeg client and turret angle

--- src/Copernicus.cpp
+++ src/Copernicus.cpp
@@ -1,74 +1,87 @@
 #include <MACE/MACE.h>
 #include <iostream>
-#include <cpprest/http_client.h>
-#include <cpprest/filestream.h>
-#include <cpprest/rawptrstream.h>
+#include <mjpgclient.h>
 
-using namespace utility;                    // Common utilities like string conversions
-using namespace web;                        // Common features like URIs.
-using namespace web::http;                  // Common HTTP functionality
-using namespace web::http::client;          // HTTP client features
-using namespace concurrency::streams;       // Asynchronous streams
+using namespace mc;
+
+gfx::Group group = gfx::Group();
+
+gfx::Group turretBase = gfx::Group();
+
+gfx::Text turretBaseDegrees;
+gfx::Image turretBaseAngle;
+
+float angle = 0;
+
+class TurretBaseComponent: public gfx::Component {
+	void init(gfx::Entity* e) {};
+	bool update(gfx::Entity* e) {
+		if( os::Input::isKeyDown(os::Input::UP) ) {
+            ++angle;
+			e->getTransformation().rotation[2] = angle * (math::pi() / 180.0f);
+            turretBaseDegrees.setText(std::to_wstring(static_cast<int>(angle))+L"°");
+		} else if( os::Input::isKeyDown(os::Input::DOWN) ) {
+            --angle;
+			e->getTransformation().rotation[2] = angle * (math::pi() / 180.0f);
+			turretBaseDegrees.setText(std::to_wstring(static_cast<int>(angle))+L"°");
+		}
+		return false;
+	};
+	void render(gfx::Entity* e) {};
+	void destroy(gfx::Entity* e) {};
+};
+
+TurretBaseComponent comp = TurretBaseComponent();
+
+void create(){
+    turretBaseAngle = gfx::Image(RESOURCE_FOLDER + std::string("/turretBase-overlay.png"));
+    turretBaseAngle.addComponent(comp);
+
+    turretBase.addChild(turretBaseAngle);
+
+    gfx::Font f = gfx::Font::loadFont(RESOURCE_FOLDER + std::string("/consola.ttf"));
+    f.setSize(24);
+    turretBaseDegrees = gfx::Text(L"0°", f);
+    turretBaseDegrees.setHorizontalAlign(gfx::HorizontalAlign::LEFT);
+    turretBaseDegrees.setVerticalAlign(gfx::VerticalAlign::TOP);
+    turretBase.addChild(turretBaseDegrees);
+
+    turretBase.setX(-0.8f);
+    turretBase.setY(-0.8f);
+    turretBase.setWidth(0.4f);
+    turretBase.setHeight(0.4f);
+
+    group.addChild(turretBase);
+}
+
+const char ip[] = "http://webcam.st-malo.com"; //Ip base of stream
+int port = 80; //Port of stream
+const char name[] = "axis-cgi/mjpg/video.cgi"; //extension
+
+MjpgClient client(ip, port, name); //Get cap line
 
 int main(int argc, char** argv){
-    char* bufferPtr = new char[1000];
+    os::WindowModule win = os::WindowModule(500, 500, "Copernicus");
+    win.setCreationCallback(&create);
 
-    rawptr_buffer<char> buffer = rawptr_buffer<char>(bufferPtr,1000);
+    win.addChild(group);
 
-   // Create http_client to send the request.
-    http_client client(U("http://webcam.st-malo.com/axis-cgi/mjpg/video.cgi"));
+    MACE::addModule(win);
 
-    // Build request URI and start the request.
-    uri_builder builder(U(""));
+    MACE::init();
 
-    // Open stream to output file.
-    pplx::task<void> requestTask = client.request(methods::GET, builder.to_string())
+    client.setDiscDim(640, 480); //Set disconnected image size
 
-    // Handle response headers arriving.
-    .then([&](http_response response)
-    {
-        if(response.status_code() != 200){
-            throw mc::InitializationError("Recieved status code of "+response.status_code());
-        }
+    while(MACE::isRunning()){
+        MACE::update();
 
-        //i like head
-        http_headers head = response.headers();
-        std::string head_find = head["Content-Type"];
+        cv::Mat curframe = client.getFrameMat(); //Test Mat (Use: getFrame for byte string)
 
-        const char* toFind = "boundary=";
+        std::cout << curframe.size() << std::endl;
 
-        size_t pos = head_find.find(toFind);
-
-        if(pos == std::string::npos) {
-            throw mc::InitializationError("Invalid mjpeg stream header");
-        }
-
-        //the stickbug
-        std::string boundary = head_find.substr(pos + sizeof(toFind)+1);
-
-        return response.body().read_line(buffer);
-
-
-        // Write response body into the file.
-      //  return response.body().read_to_end(fileStream->streambuf());
-    })
-
-    .then([&](size_t size){
-        for( mc::Index i = 0;i<size;++i){
-            std::cout<<bufferPtr[i];
-        }
-        std::cout<<std::endl;
-    });
-
-    // Wait for all the outstanding I/O to complete and handle any exceptions
-    try
-    {
-        requestTask.wait();
+        std::this_thread::sleep_for(std::chrono::milliseconds(33));
     }
-    catch (const std::exception &e)
-    {
-        printf("Error exception:%s\n", e.what());
-    }
+    MACE::destroy();
 
     return 0;
 }
